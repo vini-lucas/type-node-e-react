0d222e19c3ba349dca64a95b5d88e7a13d273e72
Lucas Vinicius <lucasvini269@gmail.com>
2026-03-19 18:01:38 -0300

    docs: notes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import express, {Request, Response} from "express";
 // Utilizando o express como funcão pode-se usar a constante app para gerenciar rotas e ligar o servidor.
 const app = express();
 
+// Cria a rota GET principal
+app.get("/", (req: Request, res: Response) => {
+    res.send("Seja bem-vindo!");
+});
+
 // Inicia o servidor na porta 8080.
 app.listen(8080, () => {
     console.log("Servidor iniciado na porta 8080.");
